68df2bd6db588003eecabc52b8de1a6ca8e5c437
nermeen143 <100959082+nermeen143@users.noreply.github.com>
2022-12-13 20:59:11 +0200

    Update Program.cs

--- OSS/Program.cs
+++ OSS/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+namespace oop;
+public abstract class Person
+{
+    public string Name;
+    public int Age;
+
+    public Staff(double name,int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    public abstract void Print();
+}
+public class Staff : Person
+{
+    public double Salary;
+    public int Joinyear;
+
+    public Student(string name,int age,double salary,int joinyear) : base(name, age)
+    {
+        Salary = salary;
+        Joinyear = joinyear;
+    }
+    public override void Print()
+    {
+        Console.WriteLine($"My name is {Name},age{Age},salary is{Salary},join year is{Joinyear}")
+    }
+}
+
+public class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("hello");
+    }
+}
